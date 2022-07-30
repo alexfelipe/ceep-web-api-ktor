acd0c207c71975672c1a97b6127de3e944abe9ce
Alex Felipe <alexfelipevieira@gmail.com>
2022-07-30 17:54:25 -0300

    initial project

---
+++ src/main/kotlin/br/com/alexf/Application.kt
@@ -0,0 +1,13 @@
+package br.com.alexf
+
+import io.ktor.server.application.*
+import br.com.alexf.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
